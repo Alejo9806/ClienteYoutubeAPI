ab7b67a5f76b7d5b5e2297b453a232ca1f0fdbf0
Alejo9806 <mello0616@gmail.com>
2021-04-04 16:58:08 -0500

    Auth with google

--- src/index.js
+++ src/index.js
@@ -1,29 +1,54 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-const ejse = require('ejs-electron')
+const { app, BrowserWindow, remote} = require('electron');
+const axios = require('axios');
+const url = require('url');
+const path = require('path');
+const ejse = require('ejs-electron');
 
 
+if(process.env.NODE_ENV != "production"){
+  require('electron-reload')(__dirname,{
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
   })
-
-  win.loadFile('./views/index.ejs')
-  win.setMenuBarVisibility(true)
 }
 
-app.whenReady().then(() => {
-  createWindow()
-  app.on('ready', () => {
-    mainWindow = new BrowserWindow()
-    mainWindow.loadURL('file://' + __dirname + './views/index.ejs')
-})
-})
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+          preload: path.join(__dirname, 'preload.js')
+        }
+  });
+  mainWindow.loadURL(url.format({
+    pathname:path.join(__dirname,'views/index.ejs'),
+    protocol:'file',
+    slashes:true
+  }))   
+  mainWindow.setMenuBarVisibility(true);
+  mainWindow.on('closed',()=>{
+    app.quit();
+  })
+});
+
+
+// function createWindow () {
+//   const win = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       preload: path.join(__dirname, 'preload.js')
+//     }
+//   })
+
+//   win.loadFile('./views/index.ejs')
+//   win.setMenuBarVisibility(true);
+// }
+
+// app.whenReady().then(() => {
+ 
+// })
 
 
 app.on('window-all-closed', () => {
