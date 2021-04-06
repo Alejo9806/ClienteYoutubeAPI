01a3218ebb073900f722ae6026c29e7b4783a056
Alejo9806 <mello0616@gmail.com>
2021-04-05 20:24:29 -0500

    AuthGoogleElectron

--- src/assets/js/auth.js
+++ src/assets/js/auth.js
@@ -1,1 +1,11 @@
+const login = document.querySelector('#logginbtn');
+const {ipcRenderer} = require('electron');
 
+login.addEventListener('click',e=>{
+    ipcRenderer.send('login');
+});
+
+ipcRenderer.on('isLogged',(e,succes)=>{
+    console.log(succes);
+    window.location.href = "./home.ejs";
+});
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+require('dotenv').config();
+
+
+'use strict';
+// const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET,GOOGLE_REDIRECT } = process.env;
 const ElectronGoogleOAuth2 = require('@getstation/electron-google-oauth2').default;
-let account= new ElectronGoogleOAuth2( "415376126316-7elok7eprsmthbt4s4mncteht332v3mc.apps.googleusercontent.com",
-"ZSKo0upSkeJj2852Jl4sJSZY", ['https://www.googleapis.com/auth/drive.metadata.readonly']);
-const { app, BrowserWindow, remote,ipcMain} = require('electron');
+let account= new ElectronGoogleOAuth2('415376126316-7elok7eprsmthbt4s4mncteht332v3mc.apps.googleusercontent.com', 'ZSKo0upSkeJj2852Jl4sJSZY', ['https://www.googleapis.com/auth/drive.metadata.readonly']);
+const { app, BrowserWindow,ipcMain} = require('electron');
 const axios = require('axios');
 const url = require('url');
 const path = require('path');
 const ejse = require('ejs-electron');
-
 
 if(process.env.NODE_ENV != "production"){
   require('electron-reload')(__dirname,{
@@ -21,7 +24,8 @@ app.on('ready', () => {
         width: 800,
         height: 600,
         webPreferences: {
-          preload: path.join(__dirname, 'preload.js')
+          nodeIntegration: true,
+          contextIsolation: false,
         }
   });
   mainWindow.loadURL(url.format({
@@ -31,22 +35,28 @@ app.on('ready', () => {
   }))   
   
   mainWindow.setMenuBarVisibility(true);
-  account.openAuthWindowAndGetTokens()
-  .then(token => {
-    console.log(token)
-    ipcMain.send('tokenGoogle',token)
-  });
-
-
 
   mainWindow.on('closed',()=>{
     app.quit();
   })
 });
 
- 
-
-
+ipcMain.on('login',(e)=>{
+    account.openAuthWindowAndGetTokens()
+    .then(token => {
+      if(token){
+        const succes ="Te has logeado correctamente amor <3"
+        console.log(succes);
+        e.reply('isLogged', succes);
+      }
+      else{
+        const succes ="Hay un error no te pudiste logear"
+        console.log(succes);
+        e.reply('isLogged',succes);
+      }
+    });
+  
+});
 
 
 
